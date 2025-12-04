d8845dcf3c0bd0b6cf45c99e56200fd5f1c28ae4
erin.major <erinmajor254@gmail.com>
2025-12-03 19:09:05 -0700

    start adding styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 import { loadHeaderFooter } from './utils.mjs';
 
 loadHeaderFooter();
+
+const menu = document.querySelector('#menu');
+const navElement = document.querySelector('#nav');
+
+menu.addEventListener('click', () => {
+
+    if (navElement.classList.contains("open")) {
+        navElement.classList.remove("open");
+        navElement.classList.add("close");
+    }
+    menu.classList.toggle("open");
+    navElement.classList.toggle("open");
+});
