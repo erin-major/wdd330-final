7aea4183c5473927d339220e893d9d3968ca3756
erin.major <erinmajor254@gmail.com>
2025-12-06 16:11:41 -0700

    add genre searching

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,27 @@
-import { searchByTitle } from './ExternalServices.mjs';
+import { searchByTitle, searchByGenre } from './ExternalServices.mjs';
 import { loadHeaderFooter } from './utils.mjs';
 
 await loadHeaderFooter();
 
-searchByTitle(1, 10, "naruto");
+const menu = document.querySelector('#menu');
+const navElement = document.querySelector('#nav');
+
+menu.addEventListener('click', () => {
+
+    if (navElement.classList.contains("open")) {
+        navElement.classList.remove("open");
+        navElement.classList.add("close");
+        menu.classList.remove("open");
+        menu.classList.add("close");
+    }
+    else {
+        navElement.classList.remove("close");
+        navElement.classList.add("open")
+        menu.classList.remove("close");
+        menu.classList.add("open");
+    }
+});
+
+const test = await searchByTitle(1, 10, "naruto");
+console.log(test);
+// searchByGenre(1, 10, "action");
