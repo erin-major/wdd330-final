505d0cf3846cd02a8d74c7f4d355a62d5b5bc1b6
erin.major <erinmajor254@gmail.com>
2025-12-06 22:44:30 -0700

    add icons

--- scripts/SearchProcess.mjs
+++ scripts/SearchProcess.mjs
@@ -64,6 +64,8 @@ export default class SearchProcess {
         card.className = 'anime-card';
 
         let title = document.createElement('h2');
+        let watchLater = document.createElement('button');
+        let watched = document.createElement('button');
         let picture = document.createElement('img');
         let genres = document.createElement('p');
         let synopsis = document.createElement('p');
@@ -71,12 +73,16 @@ export default class SearchProcess {
         title.textContent = anime.title;
         genres.textContent = anime.genres.join(", ");
         synopsis.textContent = anime.synopsis;
+        watchLater.innerHTML = '<i class="far fa-star"></i>';
+        watched.innerHTML = '<i class="far fa-check-circle" ></i >';
 
         picture.setAttribute('src', anime.image);
         picture.setAttribute('loading', 'lazy');
         picture.setAttribute('alt', anime.title);
 
         card.appendChild(title);
+        card.appendChild(watchLater);
+        card.appendChild(watched);
         card.appendChild(picture);
         card.appendChild(genres);
         card.appendChild(synopsis);
